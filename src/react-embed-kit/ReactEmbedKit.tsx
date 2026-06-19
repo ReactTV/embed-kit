4332c3ca9f453f6bba26dc5b769f256d9b6b2fc9
Quinn Elwood <ashesofowls@gmail.com>
2026-06-18 22:28:24 -0700

    Fix bug with tagReady

--- src/react-embed-kit/ReactEmbedKit.tsx
+++ src/react-embed-kit/ReactEmbedKit.tsx
@@ -161,6 +161,7 @@ export function ReactEmbedKit(props: ReactEmbedKitProps): React.ReactElement {
     onPlaybackRateChange,
     onPlaybackQualityChange,
     onCued,
+    tagReady,
   ]);
 
   const applyAttributesAndLoad = useCallback(
